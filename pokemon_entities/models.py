fc1b4167fbdc05c9e9886395509eea86e425e12c
IvanKMK <kumeyko@hotmail.com>
2020-07-26 14:59:42 +0100

    step 6 done.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     title = models.TextField()
+    photo = models.ImageField(null=True)
 
     def __str__(self):
         return '{}'.format(self.title)
