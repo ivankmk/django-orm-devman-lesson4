41433c9943cc2ca0a7639e4d0e6f4bf182445cd5
IvanKMK <kumeyko@hotmail.com>
2020-07-26 15:08:25 +0100

    step 7 done.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -10,3 +10,9 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class PokemonEntity(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    lat = models.FloatField()
+    lon = models.FloatField()
