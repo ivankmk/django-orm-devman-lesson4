9354c19da286c82b9ffed936b9512850bf8a5eca
IvanKMK <kumeyko@hotmail.com>
2020-08-03 12:27:37 +0100

    step 11 - done

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -14,17 +14,18 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
+
+    # Relationships
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
     lat = models.FloatField()
     lon = models.FloatField()
 
     appeared_at = models.DateTimeField(default=None)
-    disappeared_at = models.DateTimeField(default=None) 
+    disappeared_at = models.DateTimeField(default=None)
 
     level = models.IntegerField(default=None)
     health = models.IntegerField(default=None)
     strength = models.IntegerField(default=None)
     defence = models.IntegerField(default=None)
     stamina = models.IntegerField(default=None)
-
-    # Relationships
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
