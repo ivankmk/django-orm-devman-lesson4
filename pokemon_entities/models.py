27c27525461168b70d38ef983c29439272d4067c
IvanKMK <kumeyko@hotmail.com>
2020-08-09 17:50:28 +0100

    step 20 done.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,15 +5,19 @@ from django.db import models
 
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
-    title = models.TextField()
-    photo = models.ImageField(null=True)
-    description = models.TextField(blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    
+    title = models.TextField(verbose_name='имя')
+    photo = models.ImageField(null=True, verbose_name='изображение')
+    description = models.TextField(blank=True, verbose_name='описание')
+    title_en = models.CharField(
+        max_length=200, blank=True, verbose_name='имя на английском')
+    title_jp = models.CharField(
+        max_length=200, blank=True, verbose_name='имя на японском')
+
     # Relationships
     previous_evolution = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='next_evolutions')
+        'self', on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='next_evolutions', verbose_name='предыдущее превращение'
+    )
 
     def __str__(self):
         return '{}'.format(self.title)
@@ -24,16 +28,20 @@ class PokemonEntity(models.Model):
 
     # Relationships
     pokemon = models.ForeignKey(
-        Pokemon, on_delete=models.CASCADE, related_name='pokemon_entities')
+        Pokemon, on_delete=models.CASCADE,
+        related_name='pokemon_entities',
+        verbose_name='покемон')
 
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField(verbose_name='широта')
+    lon = models.FloatField(verbose_name='долгота')
 
-    appeared_at = models.DateTimeField(default=None)
-    disappeared_at = models.DateTimeField(default=None)
+    appeared_at = models.DateTimeField(
+        default=None, verbose_name='появился в')
+    disappeared_at = models.DateTimeField(
+        default=None, verbose_name='исчез в')
 
-    level = models.IntegerField(default=None)
-    health = models.IntegerField(default=None)
-    strength = models.IntegerField(default=None)
-    defence = models.IntegerField(default=None)
-    stamina = models.IntegerField(default=None)
+    level = models.IntegerField(default=None, verbose_name='уровень')
+    health = models.IntegerField(default=None, verbose_name='здоровье')
+    strength = models.IntegerField(default=None, verbose_name='сила')
+    defence = models.IntegerField(default=None, verbose_name='защита')
+    stamina = models.IntegerField(default=None, verbose_name='выносливость')
